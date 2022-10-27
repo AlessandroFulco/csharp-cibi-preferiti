c72b0380e9477c358d2e26988de95d7ff87c9b26
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-10-27 15:52:56 +0200

    snack-1

--- Program.cs
+++ Program.cs
@@ -10,49 +10,73 @@
 //BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. Attenzione: gestire anche il caso se aveste una classifica 
 //con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
 
-string[] cibiPreferiti = { "Pizza", "Lasagna", "Carbonara", "Tagliatelle ai funghi", "Kebab", "Tacos", "Cinese" };
+//string[] cibiPreferiti = { "Pizza", "Lasagna", "Carbonara", "Tagliatelle ai funghi", "Kebab", "Tacos", "Cinese" };
 
-//stampa lunghezza array v1
-int count = cibiPreferiti.Length;
-Console.WriteLine("La lunghezza della classsifica è: " + count);
+////stampa lunghezza array v1
+//int count = cibiPreferiti.Length;
+//Console.WriteLine("La lunghezza della classsifica è: " + count);
 
-//stampa lunghezza array v2
-Console.WriteLine("lunghezza classifica cibi: {0}", cibiPreferiti.Length);
+////stampa lunghezza array v2
+//Console.WriteLine("lunghezza classifica cibi: {0}", cibiPreferiti.Length);
 
-Console.WriteLine();
+//Console.WriteLine();
 
-// stampa lista cibi preferiti
-Console.WriteLine("La classifica dei miei cibi preferiti:");
-for (int i = 0; i < cibiPreferiti.Length; i++)
+//// stampa lista cibi preferiti
+//Console.WriteLine("La classifica dei miei cibi preferiti:");
+//for (int i = 0; i < cibiPreferiti.Length; i++)
+//{
+//    // attenzione a queste 
+//    Console.WriteLine((i + 1 + ". ") + cibiPreferiti[i]);
+//}
+
+//Console.WriteLine();
+
+////stampa il cibo top v1
+//Console.WriteLine("Il mio cibo preferito è: " + cibiPreferiti[0]);
+
+//Console.WriteLine();
+
+////stampa il cibo meno preferito
+//Console.WriteLine("Il mio cibo meno preferito è: " + cibiPreferiti[count - 1]);
+
+//Console.WriteLine();
+
+//// cibo di mezza classifica
+//if (count % 2 == 0)
+//{
+//    //v1
+//    //Console.WriteLine("I cibi di mezza classifica sono: " + cibiPreferiti[(count / 2) - 1] + " " + cibiPreferiti[count / 2]);
+
+//    //v2
+//    int medianaSuperiore = count / 2;
+//    int medianaInferiore = medianaSuperiore - 1;
+//    Console.WriteLine("I cibi di mezza classifica sono: {0} e {1}", cibiPreferiti[medianaInferiore], cibiPreferiti[medianaSuperiore]);
+
+//}
+//else
+//{
+//    Console.WriteLine("Il cibo di mezza classifica é: " + cibiPreferiti[count / 2]);
+//}
+
+
+
+//Snack1.L’utente inserisce due numeri in successione. Il software stampa il maggiore
+
+//input utente 1
+Console.Write("Inserisci un numero: ");
+int input1 = Convert.ToInt32(Console.ReadLine());
+
+//input utente 2
+Console.Write("Inserisci un numero: ");
+int input2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("I numeri selezionati sono : {0} e {1}", input1, input2);
+
+if(input1 > input2)
 {
-    // attenzione a queste 
-    Console.WriteLine( (i + 1 + ". ") + cibiPreferiti[i]);
+    Console.WriteLine(input1);
 }
-
-Console.WriteLine();
-
-//stampa il cibo top v1
-Console.WriteLine("Il mio cibo preferito è: " + cibiPreferiti[0]);
-
-Console.WriteLine();
-
-//stampa il cibo meno preferito
-Console.WriteLine("Il mio cibo meno preferito è: " + cibiPreferiti[count - 1]);
-
-Console.WriteLine();
-
-// cibo di mezza classifica
-if ( count % 2 == 0 )
+else
 {
-    //v1
-    //Console.WriteLine("I cibi di mezza classifica sono: " + cibiPreferiti[(count / 2) - 1] + " " + cibiPreferiti[count / 2]);
-
-    //v2
-    int medianaSuperiore = count / 2;
-    int medianaInferiore = medianaSuperiore - 1;
-    Console.WriteLine("I cibi di mezza classifica sono: {0} e {1}", cibiPreferiti[medianaInferiore], cibiPreferiti[medianaSuperiore]);
-
-} else
-{
-    Console.WriteLine("Il cibo di mezza classifica é: " + cibiPreferiti[count / 2]);
+    Console.WriteLine(input2);
 }
