035aa77e6ce6b31b9e1cc3daa28937368a145d3b
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-10-27 10:12:41 +0200

    finalizzazione esercizio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿
 string[] cibiPreferiti = { "Pizza", "Lasagna", "Carbonara", "Tagliatelle ai funghi", "Kebab", "Tacos", "Cinese" };
 
+//stampa lunghezza array
+int count = 0;
+
+for(; count < cibiPreferiti.Length;)
+{
+    count++;
+}
+    Console.WriteLine(count);
+
+
+// stampa lista cibi preferiti
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+//stampa il cibo top
+Console.WriteLine(cibiPreferiti[0]);
+
+//stampa il cibo meno preferito
+Console.WriteLine(cibiPreferiti[6]);
+
